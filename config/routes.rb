65a5dc26317252049a738800c5efabeedce86be8
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-30 18:49:05 +0200

    remove other links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :tweets, only: [:create]
   mount Uistiti::Engine => "/uistiti"
-  root to: 'pages#home'
-  get '/home', to: 'pages#home', as: 'home'
-  get '/contact', to: 'pages#contact', as: 'contact'
+  root to: 'pages#twitter_clone'
+  # get '/home', to: 'pages#home', as: 'home'
+  # get '/contact', to: 'pages#contact', as: 'contact'
   get '/twitter_clone', to: 'pages#twitter_clone', as: 'twitter_clone'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
